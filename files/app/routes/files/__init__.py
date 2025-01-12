67541484dd142773f423e35ef47cc3268e030894
V <v@volutega.com>
2025-01-12 12:59:26 +0300

    Обновлены подключаемые маршруты на возможность вызова без "/" на конце URL

--- files/app/routes/files/__init__.py
+++ files/app/routes/files/__init__.py
@@ -8,6 +8,6 @@ from .delete_file import delete_file
 from .update_file import update_file
 
 # Регистрация маршрутов
-files_bp.add_url_rule('/create', view_func=create_file, methods=['POST'])
-files_bp.add_url_rule('/delete', view_func=delete_file, methods=['DELETE'])
-files_bp.add_url_rule('/update', view_func=update_file, methods=['PUT'])
+files_bp.add_url_rule('/create', view_func=create_file, methods=['POST'], strict_slashes=False)
+files_bp.add_url_rule('/delete', view_func=delete_file, methods=['DELETE'], strict_slashes=False)
+files_bp.add_url_rule('/update', view_func=update_file, methods=['PUT'], strict_slashes=False)
